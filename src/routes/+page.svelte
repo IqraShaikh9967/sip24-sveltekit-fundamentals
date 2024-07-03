4d978283a6ca8664176157391a7150464a17824e
IqraShaikh9967 <Iqrask9967@gmail.com>
2024-07-03 19:58:13 +0530

    IqraShaikh-SvelteKit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,36 @@
-<h1>Welcome to Summer Internship 2024</h1>
+<header class="bg-white py-4 shadow-md sticky top-0 z-10" >
+
+<div class="container mx-auto px-4 flex justify-between items-center">
+    <h1 class="text-2xl font-bold font-['Conic_Sans_MS']">Craftlab</h1>
+    <a href="/add-post" class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">Add post</a>
+</div>
+</header>
+
+<div class="container mx-auto md:my-5">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <!-- post template -->
+        <div>
+        <img class="w-full" src="https://picsum.photos/600" alt="Post">
+        <div class="text-sm py-2 px-4">
+            <span class="font-bold">Username</span>
+            <span class="text-gray-700">Lorem ipsum dolor</span>
+        </div>
+        </div>
+
+        <div>
+            <img class="w-full" src="https://picsum.photos/600" alt="Post">
+            <div class="text-sm py-2 px-4">
+                <span class="font-bold">Username</span>
+                <span class="text-gray-700">Lorem ipsum dolor</span>
+            </div>
+            </div>
+
+            <div>
+                <img class="w-full" src="https://picsum.photos/600" alt="Post">
+                <div class="text-sm py-2 px-4">
+                    <span class="font-bold">Username</span>
+                    <span class="text-gray-700">Lorem ipsum dolor</span>
+                </div>
+                </div>
+    </div>
+</div>
